bf00c2ccf0a99047f5c25475b8923e883d5086d9
braoult <braoult@1b82040b-b2ff-0310-b578-8b0ce158f26e>
2014-11-17 16:26:19 +0000

    added thumbnails_dimensions_from_css option.
    git-svn-id: svn://svn.gna.org/svn/llgal/trunk@747 1b82040b-b2ff-0310-b578-8b0ce158f26e

--- lib/Llgal/Config.pm
+++ lib/Llgal/Config.pm
@@ -91,6 +91,7 @@ my $normal_opts_type = {
     list_links => $OPT_IS_NUMERIC,
     pixels_per_row => $OPT_IS_NUMERIC, # > 0, 0 for unlimited, -1 for default
     thumbnails_per_row => $OPT_IS_NUMERIC, # > 0, 0 for unlimited, -1 for default
+    thumbnails_dimensions_from_css => $OPT_IS_NUMERIC,
     thumbnail_height_max => $OPT_IS_NUMERIC, # > 0, -1 for default
     thumbnail_width_max => $OPT_IS_NUMERIC, # > 0, 0 for unlimited, -1 for default
     show_caption_under_thumbnails => $OPT_IS_NUMERIC,
@@ -289,6 +290,8 @@ sub add_defaults {
 	pixels_per_row => $pixels_per_row_default,
 # how many thumbnails per row at most in index (-w)
 	thumbnails_per_row => $thumbnails_per_row_default,
+# do not output thumbnails dimensions
+	thumbnails_dimensions_from_css => 0,
 # max height of a thumbnail (--ty)
 	thumbnail_height_max => $thumbnail_height_max_default,
 # scale thumb longer dimension (--tx)
